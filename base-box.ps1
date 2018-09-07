cfb35799a800168b29006fa2addfd0df135610fa
unknown <david@winslow.co.za>
2018-09-07 10:31:21 +0200

    updates

--- base-box.ps1
+++ base-box.ps1
@@ -1,14 +1,8 @@
-$userSettingsApps = @(
-    'taskbar-never-combine'
-    ,'explorer-show-all-folders'
-    ,'explorer-expand-to-current-folder'
-)
-
 $coreApps = @(
     'chocolatey'
     ,'vlc'
     ,'jre8'
-    ,'adobereader'
+    ,'adobereader-update'
     ,'googlechrome'
     ,'opera'
     ,'adblockpluschrome'
@@ -17,6 +11,8 @@ $coreApps = @(
     ,'dropbox'
     ,'youtube-dl'
     ,'googledrive'
+    ,'gotomeeting'
+    ,'icloud'
 
 )
 
@@ -38,9 +34,11 @@ $devApps = @(
         ,'sql-server-management-studio'
         ,'beyondcompare'
         ,'postman'
+        ,'git.install'
         ,'github'
         ,'sourcetree'
         ,'sysinternals'
+        ,'sourcetree'
         
         # dotnet specific
 
@@ -70,7 +68,11 @@ $Boxstarter.NoPassword=$false
 $Boxstarter.AutoLogin=$true
 
 
-
+function executeScript {
+	Param ([string]$script)
+    write-host "executing $finalBaseHelperUri/$script ..."
+	iex ((new-object net.webclient).DownloadString("https://github.com/davidfromdenmark/boxstarter/tree/master/scripts/$script"))
+}
 
 function InstallChocoApps($packageArray){
 
@@ -98,25 +100,10 @@ function InstallWindowsUpdate()
 }
 
 
-function InstallChocoDevApps
-{
-    
-    
-    InstallChocoApps $devApps
 
-    choco install git -params '"/GitAndUnixToolsOnPath"'
-    choco install sourcetree #do last since not silent
-}
 
 function InstallVisualStudio()
 {
-#    choco install visualstudio2015enterprise --source=https://www.myget.org/F/chocolatey-vs/api/v2 #kennethB is slow pushing to nuget
-#    Install-ChocolateyVsixPackage 'PowerShell Tools for Visual Studio 2015' https://visualstudiogallery.msdn.microsoft.com/c9eb3ba8-0c59-4944-9a62-6eee37294597/file/199313/1/PowerShellTools.14.0.vsix
-#    Install-ChocolateyVsixPackage 'Productivity Power Tools 2015' https://visualstudiogallery.msdn.microsoft.com/34ebc6a2-2777-421d-8914-e29c1dfa7f5d/file/169971/1/ProPowerTools.vsix
-#    
-#    Install-ChocolateyPinnedTaskBarItem "$($Boxstarter.programFiles86)\Google\Chrome\Application\chrome.exe"
-#    Install-ChocolateyPinnedTaskBarItem "$($Boxstarter.programFiles86)\Microsoft Visual Studio 14.0\Common7\IDE\devenv.exe"
-
     choco install visualstudio2017professional --package-parameters "--allWorkloads --includeRecommended --passive --locale en-US"
 }
 
@@ -182,37 +169,27 @@ function PinToTaskBar()
     choco pin add -n=sourcetree
 }
 
-
+# -------------START--------------
+Write-BoxstarterMessage "-------------START--------------"
 SetRegionalSettings
-
-# SQL Server requires some KB patches before it will work, so windows update first
 Write-BoxstarterMessage "Windows update..."
 InstallWindowsUpdate
-
-# disable chocolatey default confirmation behaviour (no need for --yes)
 choco feature enable --name=allowGlobalConfirmation
 
 
+#--- Setting up Windows ---
+executeScript "SystemConfiguration.ps1";
+executeScript "FileExplorerSettings.ps1";
+executeScript "RemoveDefaultApps.ps1";
+executeScript "VirtualizationTools.ps1";
 
 Write-BoxstarterMessage "Starting chocolatey installs"
-
-InstallChocoApps $userSettingsApps    
-
 InstallChocoApps $coreApps
+InstallChocoApps $devApps
 
-# Put last as the big SQL server / VS2017 tend to fail and kill Boxstarter it seems
-
-    Write-BoxstarterMessage "Installing Dev Apps"
-    InstallChocoDevApps
-    InstallSqlServer
-    InstallInternetInformationServices
-    InstallVisualStudio
-
+InstallVisualStudio
+InstallInternetInformationServices
 CleanDesktopShortcuts
 PinToTaskBar
 
-# Assume Windows 10
-Install-BoxstarterPackage -PackageName https://raw.githubusercontent.com/neutmute/nm-boxstarter/master/win10-clean.ps1
 
-Write-Host "Follow extra optional cleanup steps in win10-clean.ps1"
-Start-Process win10-clean.ps1
---
+++ packages/gotomeeting.install/tools/chocolateyInstall.ps1
@@ -0,0 +1,19 @@
+﻿$packageName = 'gotomeeting'
+$installerType = 'MSI'
+$url = 'https://github.com/davidfromdenmark/boxstarter/blob/master/packages/gotomeeting.install/gotomeeting.msi'
+$silentArgs = '/q ALLUSERS=1'
+$validExitCodes = @(0)
+
+try
+{
+  $tempDir = Join-Path $(Join-Path $env:TEMP "chocolatey") "$packageName"
+  $msiFile = "gotomeeting.msi"
+  $zipFile = "G2MSetup7.17.4911_IT.zip"
+  if (! [System.IO.Directory]::Exists($tempDir)) { [System.IO.Directory]::CreateDirectory($tempDir) }
+  Get-ChocolateyWebFile "$packageName" "$(Join-Path $tempDir msiFile)" $url
+  Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$(Join-Path $tempDir $msiFile)" -validExitCodes $validExitCodes
+}
+catch
+{
+  throw $_
+}

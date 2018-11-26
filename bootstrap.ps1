e634c1bf470f62c1d29d2079b09ac515e0ecbed6
davidfromdenmark <david@winslow.co.za>
2018-11-26 12:30:24 +0200

    Update bootstrap.ps1
    creating user admin if none exist

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -2,5 +2,11 @@
 get-boxstarter -Force
 
 $secpasswd = ConvertTo-SecureString "q" -AsPlainText -Force
+$op = Get-LocalUser | Where-Object {$_.Name -eq "admin"}
+if ( -not $op)
+{
+New-LocalUser "admin" -Password $secpasswd -FullName "admin" -Description "admin generated"
+Add-LocalGroupMember -Group "Administrators" -Member "admin"
 $cred = New-Object System.Management.Automation.PSCredential ("admin", $secpasswd)
+}
 Install-BoxstarterPackage -PackageName "https://raw.githubusercontent.com/davidfromdenmark/boxstarter/master/base-box.ps1" -Credential $cred
